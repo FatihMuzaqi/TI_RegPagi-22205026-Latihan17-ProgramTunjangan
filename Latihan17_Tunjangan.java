36bd8e990a2b5209c4073160f3bc682fe2121daa
FatihMuzaqi <145949071+FatihMuzaqi@users.noreply.github.com>
2023-10-28 20:33:59 +0700

    commint

---
+++ Latihan17_Tunjangan.java
@@ -0,0 +1,26 @@
+package tipedata;
+import java.util.Scanner;
+public class Latihan17_Tunjangan {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("=======Program Tunjangan========");
+        System.out.print("Berapa gaji pokok anda perbulan?:");
+        double gaji = input.nextDouble();
+        
+        System.out.print("Status Anda? (Menikah/Belum):");
+        String status = input.next();
+        
+        System.out.println("=========Hasil Perhitungan Gaji Anda===========");
+        double tunjangan = 0;
+        if (status.equalsIgnoreCase("Menikah")) {
+           tunjangan = 0.35*gaji;
+        } else { 
+            tunjangan = 0;
+        }
+        double totalgaji = gaji + tunjangan;
+        System.out.println("Gaji Pokok: "+gaji);
+        System.out.println("Tunjangan: "+tunjangan);
+        System.out.println("Total gaji: "+totalgaji);
+    }
+    
+}
